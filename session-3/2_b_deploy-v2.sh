35f1d45a4e3e22b4a69845d8bebadd1d28fc0e11
Alex Bevziuk <abevziuk@lohika.com>
2019-10-30 16:43:01 +0200

    session-3.fix

--- session-3/2_b_deploy-v2.sh
+++ session-3/2_b_deploy-v2.sh
@@ -1,13 +1,19 @@
 export VERSION=2.0
 
+# Books
 docker build -t abevziuk/books-service:${VERSION} -f books/Dockerfile books
 docker push abevziuk/books-service:${VERSION}
+kubectl delete deployments books-v2
+kubectl apply -f 2_b_deploy-books-v2.yaml
 
+# Authors
 docker build -t abevziuk/authors-service:${VERSION} -f authors/Dockerfile authors
 docker push abevziuk/authors-service:${VERSION}
-
 kubectl delete deployments authors-v2
-kubectl delete deployments books-v2
+kubectl apply -f 2_b_deploy-authors-v2.yaml
 
-kubectl apply -f deploy-authors-v2.yaml
-kubectl apply -f deploy-books-v2.yaml
+#Frontend
+docker build -t abevziuk/frontend-service:${VERSION} -f frontend/Dockerfile frontend
+docker push abevziuk/frontend-service:${VERSION}
+kubectl delete deployments frontend-v2
+kubectl apply -f 3_с_deploy-frontend-v2.yaml
